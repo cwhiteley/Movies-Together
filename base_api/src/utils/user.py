800894beefaa996128a624603454ec1dd99d5c13
MAKSIM <mburdonos@mail.ru>
2023-03-19 18:56:47 +0300

    fixed controll

--- base_api/src/utils/user.py
+++ base_api/src/utils/user.py
@@ -1,7 +1,48 @@
+from core.config import settings
 from faker import Faker
+from db.redis_cache import Redis
+import logging
+import jwt
+from fastapi.exceptions import HTTPException
 
 
-async def get_user(cokies: dict):
+def verify_token_group_view(token: str, secret_key: str, algorithms: list):
+    try:
+        payload = jwt.decode(
+            token, secret_key, algorithms=algorithms
+        )
+        return payload
+    except jwt.exceptions.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Access token expired")
+    except jwt.exceptions.DecodeError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
+def create_token_group_view(user: str):
+    access_token = jwt.encode(
+        {"user_id": user, "exp": settings.token_group_view.access_lifetime},
+        settings.token_group_view.secret_key,
+        algorithm=settings.token_group_view.algo
+    )
+    return access_token
+
+
+def get_user_and_token(cokies: dict, link: str):
+    user = None
+    if cokies.get("link"):
+        payload = verify_token_group_view(token=cokies.get("link"),
+                                          secret_key=settings.token_group_view.secret_key,
+                                          algorithms=[settings.token_group_view.algo]
+                                          )
+        return payload.get("user_id"), cokies.get("link").decode('utf-8')
     if cokies.get("access_token"):
-        return "user_form_token"
-    return Faker().first_name()
+        payload = verify_token_group_view(token=cokies.get("access_token"),
+                                          secret_key=settings.secret_key,
+                                          algorithms=[settings.token.algo]
+                                          )
+        user = payload.get("user_id")
+    if not user:
+        user = Faker().first_name()
+    token = create_token_group_view(user)
+    return user, token.decode('utf-8')
+
